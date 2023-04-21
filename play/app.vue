65d1e2887f57129763cad0e0fa8c3b562d55696b
胡富强 <hufuqiang@daojia-inc.com>
2023-04-21 14:03:35 +0800

    init

---
+++ play/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ea-button type="primary">主要按钮</ea-button>
+    <Icon/>
+  </div>
+</template>
+<script lang="ts" setup>
+// import { Button, Icon } from "@easyest/components";
+</script>
---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,49 @@
+export default {
+  themeConfig: {
+    siteTitle: "vitepress",
+    nav: [
+      { text: "指南", link: "/guild/installation/" },
+      { text: "组件", link: "/components/button/" },
+    ],
+    socialLinks: [
+      { icon: "gitlab", link: "http://git.kg-inc.cn/fe-sy-lahuo/driver-vue3-components" },
+    ],
+    sidebar: {
+      "/guild/": [
+        {
+          text: "基础",
+          items: [
+            {
+              text: "安装",
+              link: "/guild/installation/",
+            },
+            {
+              text: "快速开始",
+              link: "/guild/quickstart/",
+            },
+          ],
+        },
+        {
+          text: "进阶",
+          items: [
+            {
+              text: "xx",
+              link: "/xx",
+            },
+          ],
+        },
+      ],
+      "/components/": [
+        {
+          text: "基础组件",
+          items: [
+            {
+              text: "Button",
+              link: "/components/button/",
+            }
+          ],
+        }
+      ]
+    },
+  },
+};
